ed571697a2bc3ced4ed8226fa0d155594aa5dbc5
Pintu-Roy121 <roypintu121@gmail.com>
2022-10-11 20:43:41 +0600

    blog added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is Blog Page!</h1>
+        <div className='w-4/5 mx-auto my-20'>
+            <div className='text-center bg-slate-300 rounded-lg'>
+                <h1 className='text-4xl font-bold text-black my-3 rounded-top-lg shadow-md shadow-cyan-600 py-2'>What is the purpose of React Router?</h1>
+                <p className='text-lg font-medium text-justify text-black w-4/5 mx-auto pb-8 mt-5'>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL. </p>
+            </div>
+            <div className='text-center bg-slate-300 rounded-lg'>
+                <h1 className='text-4xl font-bold text-black my-3 rounded-top-lg shadow-md shadow-cyan-600 py-2'>How does context api work?</h1>
+                <p className='text-lg font-medium text-justify text-black w-4/5 mx-auto pb-8 mt-5'>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+            </div>
+            <div className='text-center bg-slate-300 rounded-lg'>
+                <h1 className='text-4xl font-bold text-black my-3 rounded-top-lg shadow-md shadow-cyan-600 py-2'>About useRef() hook in Reactjs. </h1>
+                <p className='text-lg font-medium text-justify text-black w-4/5 mx-auto pb-8 mt-5'><span className='bg-orange-100 px-2'>useRef()</span> returns a mutable ref object whose <span className='bg-orange-100 px-2'>.current</span> property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.Essentially, useRef is like a “box” that can hold a mutable value in its .current property. <br />If you pass a ref object to React with <span className='bg-orange-100 px-2'> (ref=myRef)</span>, React will set its .current property to the corresponding DOM node whenever that node changes.</p>
+            </div>
         </div>
     );
 };
--- src/components/SingleQuestion/SingleQuestion.js
+++ src/components/SingleQuestion/SingleQuestion.js
@@ -8,7 +8,6 @@ const SingleQuestion = ({ quizes }) => {
     const { question, options, correctAnswer } = quizes;
 
     const showCorrectAnswer = () => {
-
         Swal.fire({
             title: 'Correct Answer is:',
             text: correctAnswer
@@ -17,7 +16,7 @@ const SingleQuestion = ({ quizes }) => {
 
     return (
         <div className='lg:mx-60 mt-12 flex flex-col gap-12'>
-            <div className="bg-gray-50 px-10 lg:py-5 relative rounded-lg">
+            <div className="bg-gray-100 px-10 lg:py-5 relative rounded-lg  shadow-lg shadow-cyan-800">
                 <EyeIcon onClick={showCorrectAnswer} className='w-7 h-7 text-blue-600 absolute top-5 right-10' />
                 <h2 className="card-title justify-around mt-7 text-cyan-500">Quize: {question}</h2>
                 <div className='grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-6 justify-between my-10'>
