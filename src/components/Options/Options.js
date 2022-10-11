88b794da516cefd56bff2bf680bb590b309a66d9
Pintu-Roy121 <roypintu121@gmail.com>
2022-10-11 15:06:54 +0600

    eye icon fixed

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Options = ({ option }) => {
-    // console.log(option);
+const Options = ({ quizes, option }) => {
+    // console.log(quizes);
+    const { correctAnswer } = quizes;
+    const handleClick = (option) => {
+        if (option === correctAnswer) {
+            console.log('clickd');
+            // showCorrectAnswer(option);
+        }
+    }
     return (
         <div>
-            <p className='border-2 border-gray-400 rounded-lg w-full py-2 flex items-center justify-center px-2 h-28j text-sm md:text-base'> {option}</p>
+            <button onClick={() => handleClick(option)} className='border-2 border-gray-600 bg-slate-50 rounded-lg w-full py-2 flex items-center justify-center px-2 h-28 j text-sm md:text-base'><p> {option}</p></button>
         </div>
     );
 };
--- src/components/SingleQuestion/SingleQuestion.js
+++ src/components/SingleQuestion/SingleQuestion.js
@@ -4,16 +4,25 @@ import Options from '../Options/Options'
 
 const SingleQuestion = ({ quizes }) => {
     // console.log(quizes);
-    const { question, options } = quizes;
+    const { question, options, correctAnswer } = quizes;
     // console.log(options);
+    // const handleClick = (option) => {
+    //     console.log(typeof option);
+    // }
+    const showCorrectAnswer = () => {
+        // console.log(question.correctAnswer);
+        console.log(quizes.correctAnswer);
+        alert(correctAnswer)
+    }
     return (
         <div className='lg:mx-60 mt-12 flex flex-col gap-12'>
             <div className="bg-gray-50 px-10 lg:py-5 relative rounded-lg">
-                <EyeIcon className='w-7 h-7 text-blue-600 absolute top-5 right-10' />
+                <EyeIcon onClick={showCorrectAnswer} className='w-7 h-7 text-blue-600 absolute top-5 right-10' />
                 <h2 className="card-title justify-around mt-7 text-cyan-500">Quize: {question}</h2>
-                <div className='grid grid-cols-1 gap-2 lg:grid-cols-2 lg:gap-6 justify-between my-10'>
+                <div className='grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-6 justify-between my-10'>
                     {
                         options.map((option, index) => <Options
+                            quizes={quizes}
                             key={index}
                             option={option}
                         />)
